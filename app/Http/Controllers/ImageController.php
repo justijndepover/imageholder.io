08d9d606bfd18f080515a7ec8717e187bf1ef11c
= <justijndepover@gmail.com>
2020-08-29 18:34:47 +0200

    all page is pretty 😻

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -14,7 +14,13 @@ class ImageController extends Controller
             $height = $width;
         }
 
-        $image = collect(Storage::allFiles('/images'))->random();
+        // This can be improved
+        if ($request->image && (int) $request->image < 6) {
+            $image = "images/image-{$request->image}.jpg";
+        } else {
+            $image = collect(Storage::allFiles('/images'))->random();
+        }
+
         $cropped = Image::make(storage_path('app/') . $image)->fit($width, $height);
         $extension = ($request->extension && in_array($request->extension, ['png', 'jpeg', 'jpg'])) ? $request->extension : 'jpeg';
 
--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -3,5 +3,12 @@
 @section('content')
     <h1 class="text-6xl font-black leading-none mb-12">All our images.</h1>
 
-    <img src="{{ route('image', ['width' => 1000, 'height' => 800]) }}" alt="example image" class="rounded-lg" width="1000" height="800">
+    <div class="flex flex-wrap -m-2">
+        @foreach (range(1, 5) as $number)
+            <div class="sm:w-1/2 p-2">
+                <img src="{{ route('image', ['width' => 1000, 'height' => 800, 'image' => $number]) }}" alt="example image" class="rounded-lg" width="1000" height="800">
+            </div>
+        @endforeach
+    </div>
+
 @endsection
