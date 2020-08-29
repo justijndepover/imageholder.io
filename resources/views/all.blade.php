4a160344576c04b51438efb526cd1e9a70291c4d
= <justijndepover@gmail.com>
2020-08-29 18:03:47 +0200

    added all page (wip)

---
+++ resources/views/all.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+    <h1 class="text-6xl font-black leading-none mb-12">All our images.</h1>
+
+    <img src="{{ route('image', ['width' => 1000, 'height' => 800]) }}" alt="example image" class="rounded-lg">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', HomeController::class)->name('home');
+Route::get('all', ImagesController::class)->name('all');
+
 Route::get('{width}', ImageController::class)->name('image.square');
 Route::get('{width}/{height}', ImageController::class)->name('image');
-
-Route::get('/', HomeController::class)->name('home');
