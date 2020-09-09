c72afb8a7654ff7fe022d71d94fff1e83af718d3
= <justijndepover@gmail.com>
2020-09-09 22:08:24 +0200

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,15 +7,35 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function it_should_display_homepage()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_should_throw_422_on_incorrect_image_dimension()
+    {
+        $response = $this->get('/foo');
+
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function it_should_throw_422_on_incorrect_image_dimensions()
+    {
+        $response = $this->get('/foo/bar');
+
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function it_should_return_a_correct_image_format()
+    {
+        $response = $this->get('/400/500');
+
+        $this->assertTrue(false);
     }
 }
